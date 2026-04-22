9d6c9c80649049bc489cd2974f88224851dd42a2
joshj <joshj@JOSHJVR>
2026-04-22 15:41:23 +0200

    I had created the layout for the design of the Form(Ticketing System) keeping to the layout of the given layout as provided in the question sheet and had then moved onto creating the logic, for the logic i had wanted to add validation for the fields as to protect from user error and had also added the needed pricing and discount rules where they may apply.The code is somewhat completed and will checked for errors.

--- TicketApplicationSystem/Form1.Designer.cs
+++ TicketApplicationSystem/Form1.Designer.cs
@@ -2,15 +2,8 @@
 {
     partial class Form1
     {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -20,21 +13,172 @@
             base.Dispose(disposing);
         }
 
-        #region Windows Form Designer generated code
-
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.lblTicketApplication = new System.Windows.Forms.Label();
+            this.lblName = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.lblGender = new System.Windows.Forms.Label();
+            this.rbtnMale = new System.Windows.Forms.RadioButton();
+            this.rbtnFemale = new System.Windows.Forms.RadioButton();
+            this.lblAge = new System.Windows.Forms.Label();
+            this.txtAge = new System.Windows.Forms.TextBox();
+            this.lblCategory = new System.Windows.Forms.Label();
+            this.cboCategory = new System.Windows.Forms.ComboBox();
+            this.lblDistance = new System.Windows.Forms.Label();
+            this.txtDistance = new System.Windows.Forms.TextBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+
+            // Form1
+            this.Text = "Ticketing System";
+            this.Size = new System.Drawing.Size(480, 500);
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular);
+
+            // lblTitle
+            this.lblTitle.Text = "Ticketing System";
+            this.lblTitle.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold);
+            this.lblTitle.ForeColor = System.Drawing.Color.DarkSlateBlue;
+            this.lblTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblTitle.Dock = System.Windows.Forms.DockStyle.Top;
+            this.lblTitle.Height = 50;
+            this.lblTitle.BackColor = System.Drawing.Color.LightSteelBlue;
+
+            // lblTicketApplication
+            this.lblTicketApplication.Text = "TICKET APPLICATION";
+            this.lblTicketApplication.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
+            this.lblTicketApplication.Location = new System.Drawing.Point(30, 70);
+            this.lblTicketApplication.Size = new System.Drawing.Size(400, 25);
+            this.lblTicketApplication.ForeColor = System.Drawing.Color.Navy;
+
+            // lblName
+            this.lblName.Text = "Name";
+            this.lblName.Location = new System.Drawing.Point(30, 110);
+            this.lblName.Size = new System.Drawing.Size(100, 25);
+
+            // txtName
+            this.txtName.Location = new System.Drawing.Point(150, 108);
+            this.txtName.Size = new System.Drawing.Size(250, 25);
+            this.txtName.Name = "txtName";
+
+            // lblGender
+            this.lblGender.Text = "Gender";
+            this.lblGender.Location = new System.Drawing.Point(30, 150);
+            this.lblGender.Size = new System.Drawing.Size(100, 25);
+
+            // rbtnMale
+            this.rbtnMale.Text = "MALE";
+            this.rbtnMale.Location = new System.Drawing.Point(150, 148);
+            this.rbtnMale.Size = new System.Drawing.Size(80, 25);
+            this.rbtnMale.Name = "rbtnMale";
+
+            // rbtnFemale
+            this.rbtnFemale.Text = "FEMALE";
+            this.rbtnFemale.Location = new System.Drawing.Point(240, 148);
+            this.rbtnFemale.Size = new System.Drawing.Size(90, 25);
+            this.rbtnFemale.Name = "rbtnFemale";
+
+            // lblAge
+            this.lblAge.Text = "Age";
+            this.lblAge.Location = new System.Drawing.Point(30, 190);
+            this.lblAge.Size = new System.Drawing.Size(100, 25);
+
+            // txtAge
+            this.txtAge.Location = new System.Drawing.Point(150, 188);
+            this.txtAge.Size = new System.Drawing.Size(100, 25);
+            this.txtAge.Name = "txtAge";
+
+            // lblCategory
+            this.lblCategory.Text = "Category";
+            this.lblCategory.Location = new System.Drawing.Point(30, 230);
+            this.lblCategory.Size = new System.Drawing.Size(100, 25);
+
+            // cboCategory
+            this.cboCategory.Location = new System.Drawing.Point(150, 228);
+            this.cboCategory.Size = new System.Drawing.Size(150, 25);
+            this.cboCategory.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboCategory.Name = "cboCategory";
+            this.cboCategory.Items.AddRange(new object[] { "Category One", "Category Two", "Category Three" });
+
+            // lblDistance
+            this.lblDistance.Text = "Distance";
+            this.lblDistance.Location = new System.Drawing.Point(30, 270);
+            this.lblDistance.Size = new System.Drawing.Size(100, 25);
+
+            // txtDistance
+            this.txtDistance.Location = new System.Drawing.Point(150, 268);
+            this.txtDistance.Size = new System.Drawing.Size(100, 25);
+            this.txtDistance.Name = "txtDistance";
+
+            // btnCalculate
+            this.btnCalculate.Text = "CALCULATE";
+            this.btnCalculate.Location = new System.Drawing.Point(60, 330);
+            this.btnCalculate.Size = new System.Drawing.Size(100, 35);
+            this.btnCalculate.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Click += new System.EventHandler(this.BtnCalculate_Click);
+
+            // btnClear
+            this.btnClear.Text = "CLEAR";
+            this.btnClear.Location = new System.Drawing.Point(190, 330);
+            this.btnClear.Size = new System.Drawing.Size(100, 35);
+            this.btnClear.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Click += new System.EventHandler(this.BtnClear_Click);
+
+            // btnExit
+            this.btnExit.Text = "EXIT";
+            this.btnExit.Location = new System.Drawing.Point(320, 330);
+            this.btnExit.Size = new System.Drawing.Size(100, 35);
+            this.btnExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Click += new System.EventHandler(this.BtnExit_Click);
+
+            // Add controls to form
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.lblTicketApplication);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.lblGender);
+            this.Controls.Add(this.rbtnMale);
+            this.Controls.Add(this.rbtnFemale);
+            this.Controls.Add(this.lblAge);
+            this.Controls.Add(this.txtAge);
+            this.Controls.Add(this.lblCategory);
+            this.Controls.Add(this.cboCategory);
+            this.Controls.Add(this.lblDistance);
+            this.Controls.Add(this.txtDistance);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnExit);
+
+            this.ResumeLayout(false);
+            this.PerformLayout();
         }
 
-        #endregion
+        // Control declarations 
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label lblTicketApplication;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label lblGender;
+        private System.Windows.Forms.RadioButton rbtnMale;
+        private System.Windows.Forms.RadioButton rbtnFemale;
+        private System.Windows.Forms.Label lblAge;
+        private System.Windows.Forms.TextBox txtAge;
+        private System.Windows.Forms.Label lblCategory;
+        private System.Windows.Forms.ComboBox cboCategory;
+        private System.Windows.Forms.Label lblDistance;
+        private System.Windows.Forms.TextBox txtDistance;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnExit;
     }
 }
-
--- TicketApplicationSystem/Form1.cs
+++ TicketApplicationSystem/Form1.cs
@@ -1,12 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.Linq;
 
 namespace TicketApplicationSystem
 {
@@ -14,7 +8,121 @@ namespace TicketApplicationSystem
     {
         public Form1()
         {
-            InitializeComponent();
+            InitializeComponent(); 
+        }
+
+        
+        private void BtnCalculate_Click(object sender, EventArgs e)
+        {
+            if (!ValidateInputs())
+                return;
+
+            string selectedCategory = cboCategory.SelectedItem.ToString();
+            double ratePerKm = GetRatePerKm(selectedCategory);
+            double distance = double.Parse(txtDistance.Text);
+            int age = int.Parse(txtAge.Text);
+            double basePrice = ratePerKm * distance;
+            double finalPrice;
+            string discountMessage;
+
+            if (age < 12)
+            {
+                finalPrice = 0;
+                discountMessage = "FREE ticket (Age < 12)";
+            }
+            else if (rbtnFemale.Checked)
+            {
+                finalPrice = basePrice * 0.5;
+                discountMessage = "50% Female discount applied";
+            }
+            else
+            {
+                finalPrice = basePrice;
+                discountMessage = "No discount";
+            }
+
+            string gender = rbtnMale.Checked ? "MALE" : "FEMALE";
+            string summary = $"TICKET SUMMARY\n--------------------------------\n" +
+                              $"Passenger Name : {txtName.Text.Trim()}\n" +
+                              $"Gender         : {gender}\n" +
+                              $"Age               : {age}\n" +
+                              $"Category       : {selectedCategory}\n" +
+                              $"Distance       : {distance} km\n" +
+                              $"--------------------------------\n" +
+                              $"Base Price     : R {basePrice:F2}\n" +
+                              $"Discount       : {discountMessage}\n" +
+                              $"FINAL PRICE  : R {finalPrice:F2}\n" +
+                              $"--------------------------------";
+
+            MessageBox.Show(summary, "Ticket Summary", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private double GetRatePerKm(string category)
+        {
+            switch (category)
+            {
+                case "Category One": return 20;
+                case "Category Two": return 35;
+                case "Category Three": return 50;
+                default: return 0;
+            }
+        }
+
+        private bool ValidateInputs()
+        {
+            if (string.IsNullOrWhiteSpace(txtName.Text))
+            {
+                MessageBox.Show("Please enter passenger name.", "Validation Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txtName.Focus();
+                return false;
+            }
+
+            if (!rbtnMale.Checked && !rbtnFemale.Checked)
+            {
+                MessageBox.Show("Please select gender (MALE or FEMALE).", "Validation Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return false;
+            }
+
+            if (!int.TryParse(txtAge.Text, out int age) || age <= 0 || age > 120)
+            {
+                MessageBox.Show("Please enter a valid age (1-120).", "Validation Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txtAge.Clear();
+                txtAge.Focus();
+                return false;
+            }
+
+            if (!double.TryParse(txtDistance.Text, out double distance) || distance <= 0)
+            {
+                MessageBox.Show("Please enter a valid distance (> 0 km).", "Validation Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txtDistance.Clear();
+                txtDistance.Focus();
+                return false;
+            }
+
+            if (cboCategory.SelectedIndex == -1)
+            {
+                MessageBox.Show("Please select a travel category.", "Validation Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                cboCategory.Focus();
+                return false;
+            }
+
+            return true;
+        }
+
+        private void BtnClear_Click(object sender, EventArgs e)
+        {
+            txtName.Clear();
+            rbtnMale.Checked = false;
+            rbtnFemale.Checked = false;
+            txtAge.Clear();
+            cboCategory.SelectedIndex = -1;
+            txtDistance.Clear();
+            txtName.Focus();
+        }
+
+        private void BtnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
         }
     }
 }
